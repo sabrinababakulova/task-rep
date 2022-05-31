454b6f2f6316aaa3f18af4817479c258bc907d61
sabrina34352 <s.babakulova@student.inha.uz>
2022-05-31 14:27:59 +0500

    eslintrc and HOC improved

--- src/components/withLoadingDelay/index.jsx
+++ src/components/withLoadingDelay/index.jsx
@@ -1,29 +1,20 @@
 import React, { useState, useEffect } from 'react'
 import { Spinner, Box } from '@chakra-ui/react'
 
-const withLoadingDelay = (Component) => {
-    const HOC = (props) => {
-        const [loading, setLoading] = useState(true)
-        useEffect(() => {
-            setTimeout(() => {
-                setLoading(false)
-            }, 2000)
-        }, [])
+const withLoadingDelay = (Component) => (props) => {
+    const [loading, setLoading] = useState(true)
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false)
+        }, 2000)
+    }, [])
 
-        return loading ? (
-            <Box
-                boxShadow="base"
-                p="6"
-                mb="4"
-                w={['xs', 'sm', 'lg']}
-                bg="gray.100"
-            >
-                <Spinner size="xl" color="teal" thickness="4px" />
-            </Box>
-        ) : (
-            <Component {...props} />
-        )
-    }
-    return HOC
+    return loading ? (
+        <Box boxShadow="base" p="6" mb="4" w={['xs', 'sm', 'lg']} bg="gray.100">
+            <Spinner size="xl" color="teal" thickness="4px" />
+        </Box>
+    ) : (
+        <Component {...props} />
+    )
 }
 export default withLoadingDelay
