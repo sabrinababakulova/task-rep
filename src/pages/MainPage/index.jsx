69bc281bdcf385069f42e884d76f660721cf5998
sabrina34352 <s.babakulova@student.inha.uz>
2022-09-14 10:08:19 +0500

    custom hook added

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,8 +1,9 @@
 import React, { Suspense } from 'react';
 const CardList = React.lazy(() => import('../../components/CardList'));
 const FuncButtons = React.lazy(() => import('../../components/FuncButtons'));
-
+import usePromptHook from '../../hooks/usePromptHook';
 const MainPage = () => {
+  usePromptHook("are you still logged in? ", 10000);
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <FuncButtons />
